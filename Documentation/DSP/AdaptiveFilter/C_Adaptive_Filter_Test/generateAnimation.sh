7c13364b997089e9786cb8986f3cb0fa2364f832
4n3m4i1 <adrian.de.vico@gmail.com>
2023-10-09 12:05:25 -0700

    Fixed weird gif colors w/ dynamic ffmpeg palettes

--- Documentation/DSP/AdaptiveFilter/C_Adaptive_Filter_Test/generateAnimation.sh
+++ Documentation/DSP/AdaptiveFilter/C_Adaptive_Filter_Test/generateAnimation.sh
@@ -5,5 +5,7 @@
 python ./generate_png_from_csv.py adaptive_tap_frames.csv
 
 cd ./oi4v
-ffmpeg -hide_banner -f image2 -framerate 24 -i iter_frame_%d.png -loop 0 anim_output.gif
+ffmpeg -i iter_frame_%d.png -vf palettegen palette.png
+ffmpeg -i image_%02d.png -i palette.png -lavfi paletteuse video.gif
+ffmpeg -hide_banner -thread_queue_size 1024 -f image2 -framerate 24 -i iter_frame_%d.png -i palette.png -lavfi paletteuse -loop 0 anim_output.gif
 cd ..
