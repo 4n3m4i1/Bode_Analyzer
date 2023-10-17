529a6997211a6e62cf01762d56bd9b722856f308
4n3m4i1 <adrian.de.vico@gmail.com>
2023-10-17 13:29:56 -0700

    Small tool for double -> Q15 conversion, need to finish file handling part

---
+++ src/C_Utilities/Float_2_Q15_Converter/README.md
@@ -0,0 +1,5 @@
+# Float to Q15 Converter
+Converts a float or double to Q15 by default.
+
+## Command Line Options
+-f {file name}: Converts an input list of newline seperated doubles to Q15 hex format
---
+++ src/C_Utilities/Float_2_Q15_Converter/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FIX_2_FLOAT
+#include "../generic_include.h"
+
+
+
+void print_val_as_Q15(double in);
+
+void main(int argc, char **argv){
+    if(argc == 2){  // Calc input val
+        double ab = strtod(argv[1],NULL);
+        print_val_as_Q15(ab);
+    } else
+    if(argc > 2){
+        printf("Not implemented yet :)\n");
+    }
+}
+
+
+
+void print_val_as_Q15(double in){
+    printf("Entered:      %.14lf\n", in);
+    Q15 aa = float_2_Q15(in);
+    ///char tmp[8] = {0x00};
+    //sprintf(tmp,"%04X",aa);
+    printf("Float -> Q15: 0x%04X\n", aa);
+    float bcv = Q15_2_float(aa);
+    printf("Q15 -> Float: %.14lf\n", bcv);
+    printf("Abs Error:    %.14lf\n", in - bcv);
+    printf("%% Error:      %.4lf%%\n", 100 * ((bcv - in) / in));
+
+}
