a43e8d79213ec7e9978944c7e504c11c0de09f58
4n3m4i1 <adrian.de.vico@gmail.com>
2023-10-26 12:06:43 -0700

    Swap to struct and dynamic structure for variable length

--- Documentation/DSP/FFT_Fixed_Pt/FFT.h
+++ Documentation/DSP/FFT_Fixed_Pt/FFT.h
@@ -5,10 +5,10 @@
 #include "generic_include.h"
 #include "Q15_sin_table.h"
 
-#define NUM_SAMPLES         64
-#define NUM_SAMPLES_M_1     63
-#define LOG2_NUM_SAMPLES    6
-#define SHIFT_AMOUNT        ((sizeof(Q15) * 8) - LOG2_NUM_SAMPLES)
+//#define NUM_SAMPLES         64
+//#define NUM_SAMPLES_M_1     64
+//#define LOG2_NUM_SAMPLES    6
+//#define SHIFT_AMOUNT        ((sizeof(Q15) * 8) - LOG2_NUM_SAMPLES)
 // #define LOG2_N_WAVE 10
 
 
@@ -20,16 +20,35 @@
 Q15 zero_point_4 = float_2_Q15(0.4) ;
 
 // Here's where we'll have the DMA channel put ADC samples
-uint8_t sample_array[NUM_SAMPLES] ;
-// And here's where we'll copy those samples for FFT calculation
-Q15 fr[NUM_SAMPLES] ;
-Q15 fi[NUM_SAMPLES] ;
+//uint8_t sample_array[NUM_SAMPLES] ;
+//// And here's where we'll copy those samples for FFT calculation
+//Q15 fr[NUM_SAMPLES] ;
+//Q15 fi[NUM_SAMPLES] ;
 
-//Q15 sin_table[NUM_SAMPLES]; // a table of sines for the FFT
-//Q15 fr[NUM_SAMPLES], fi[NUM_SAMPLES];
+struct FFT_PARAMS {
+    uint16_t    num_samples;
+    uint16_t    log2_num_samples;
+    uint16_t    shift_amount;
+
+    Q15 *fr;
+    Q15 *fi;
+};
 
 
-void FFTfix(Q15 *fr, Q15 *fi) {
+uint16_t get_log_2(uint16_t num_samples){
+    for(uint16_t n = 0; n < 16; n++){
+        num_samples >>= 1;
+        if(!num_samples) return (n);
+    }
+    return 0;
+}
+
+uint16_t get_shift_amt(uint16_t log2ns){
+    return ((sizeof(Q15) * 8) - log2ns);
+}
+
+
+void FFT_fixdpt(struct FFT_PARAMS *fft) {
 
     unsigned short m;   // one of the indices being swapped
     unsigned short mr ; // the other index being swapped (r for reversed)
@@ -49,7 +68,7 @@ void FFTfix(Q15 *fr, Q15 *fi) {
     //////////////////////////////////////////////////////////////////////////
     // Bit reversal code below based on that found here: 
     // https://graphics.stanford.edu/~seander/bithacks.html#BitReverseObvious
-    for (m=1; m<NUM_SAMPLES_M_1; m++) {
+    for (m = 1; m < fft->num_samples; ++m) {
         // swap odd and even bits
         mr = ((m >> 1) & 0x5555) | ((m & 0x5555) << 1);
         // swap consecutive pairs
@@ -59,16 +78,16 @@ void FFTfix(Q15 *fr, Q15 *fi) {
         // swap bytes
         mr = ((mr >> 8) & 0x00FF) | ((mr & 0x00FF) << 8);
         // shift down mr
-        mr >>= SHIFT_AMOUNT ;
+        mr >>= fft->shift_amount;
         // don't swap that which has already been swapped
         if (mr<=m) continue ;
         // swap the bit-reveresed indices
-        tr = fr[m] ;
-        fr[m] = fr[mr] ;
-        fr[mr] = tr ;
-        ti = fi[m] ;
-        fi[m] = fi[mr] ;
-        fi[mr] = ti ;
+        tr = fft->fr[m] ;
+        fft->fr[m] = fft->fr[mr] ;
+        fft->fr[mr] = tr ;
+        ti = fft->fi[m] ;
+        fft->fi[m] = fft->fi[mr] ;
+        fft->fi[mr] = ti ;
     }
 
     //////////////////////////////////////////////////////////////////////////
@@ -79,34 +98,34 @@ void FFTfix(Q15 *fr, Q15 *fi) {
     // Length of the FFT's being combined (starts at 1)
     L = 1 ;
     // Log2 of number of samples, minus 1
-    k = LOG2_NUM_SAMPLES - 1 ;
+    k = fft->log2_num_samples - 1 ;
     // While the length of the FFT's being combined is less than the number of gathered samples
-    while (L < NUM_SAMPLES) {
+    while (L < fft->num_samples) {
         // Determine the length of the FFT which will result from combining two FFT's
         istep = L<<1 ;
         // For each element in the FFT's that are being combined . . .
         for (m=0; m<L; ++m) { 
             // Lookup the trig values for that element
             j = m << k ;                         // index of the sine table
-            wr =  sin_table[j + NUM_SAMPLES/4] ; // cos(2pi m/N)
+            wr =  sin_table[j + (fft->num_samples >> 2)] ; // cos(2pi m/N)
             wi = -sin_table[j] ;                 // sin(2pi m/N)
             wr >>= 1 ;                          // divide by two
             wi >>= 1 ;                          // divide by two
             // i gets the index of one of the FFT elements being combined
-            for (i=m; i<NUM_SAMPLES; i+=istep) {
+            for (i = m; i < fft->num_samples; i += istep) {
                 // j gets the index of the FFT element being combined with i
                 j = i + L ;
                 // compute the trig terms (bottom half of the above matrix)
-                tr = mul_Q15(wr, fr[j]) - mul_Q15(wi, fi[j]) ;
-                ti = mul_Q15(wr, fi[j]) + mul_Q15(wi, fr[j]) ;
+                tr = mul_Q15(wr, fft->fr[j]) - mul_Q15(wi, fft->fi[j]) ;
+                ti = mul_Q15(wr, fft->fi[j]) + mul_Q15(wi, fft->fr[j]) ;
                 // divide ith index elements by two (top half of above matrix)
-                qr = fr[i]>>1 ;
-                qi = fi[i]>>1 ;
+                qr = fft->fr[i] >> 1 ;
+                qi = fft->fi[i] >> 1 ;
                 // compute the new values at each index
-                fr[j] = qr - tr ;
-                fi[j] = qi - ti ;
-                fr[i] = qr + tr ;
-                fi[i] = qi + ti ;
+                fft->fr[j] = qr - tr ;
+                fft->fi[j] = qi - ti ;
+                fft->fr[i] = qr + tr ;
+                fft->fi[i] = qi + ti ;
             }    
         }
         --k ;
@@ -115,21 +134,20 @@ void FFTfix(Q15 *fr, Q15 *fi) {
 }
 
 
-int FFTmag(Q15 *fr, Q15 *fi){
+int FFT_mag(struct FFT_PARAMS *fft){
     Q15 max_fr = 0;
     int max_fr_dex = -1;
 
-    for (int i = 0; i < (NUM_SAMPLES>>1); i++) {  
+    for (int i = 0; i < (fft->num_samples >> 1); i++) {  
         // get the approx magnitude
-        fr[i] = abs(fr[i]); //>>9
-        fi[i] = abs(fi[i]);
+        fft->fr[i] = abs(fft->fr[i]); //>>9
+        //fft->fi[i] = abs(fft->fi[i]);
         // reuse fr to hold magnitude
-        fr[i] = max(fr[i], fi[i]) + 
-                mul_Q15(min(fr[i], fi[i]), zero_point_4); 
+        fft->fr[i] = max(fft->fr[i], abs(fft->fi[i])) + mul_Q15(min(fft->fr[i], abs(fft->fi[i])), zero_point_4); 
 
         // Keep track of maximum
-        if ((fr[i] > max_fr) && (i > 0)) {
-            max_fr = fr[i] ;
+        if ((fft->fr[i] > max_fr) && (i > 0)) {
+            max_fr = fft->fr[i] ;
             max_fr_dex = i ;
         }
     }
--- Documentation/DSP/FFT_Fixed_Pt/main.c
+++ Documentation/DSP/FFT_Fixed_Pt/main.c
@@ -10,20 +10,36 @@
 double dfl_scale = 2.0;
 double amp_scale = 1.0;
 
+#define NUM_SAMPLES 64
+
 void main(int argc, char **argv){
+    struct FFT_PARAMS fft;
+    fft.num_samples = NUM_SAMPLES;
+    fft.log2_num_samples = get_log_2(NUM_SAMPLES);
+    fft.shift_amount = get_shift_amt(fft.log2_num_samples);
+
+    printf("Shift Amt: %u\n",fft.log2_num_samples);
+    printf("Shift Amt: %u\n",fft.shift_amount);
+
+    Q15 ffr[NUM_SAMPLES];
+    Q15 ffi[NUM_SAMPLES];
+
+    fft.fr = ffr;
+    fft.fi = ffi;
+    
     if(argc > 1) dfl_scale = strtod(argv[1],NULL);
     if(argc > 2) amp_scale = strtod(argv[2],NULL);
 
     for(int n = 0; n < NUM_SAMPLES; ++n){
-        fr[n] = float_2_Q15(amp_scale * sin(dfl_scale * 6.283 * ((float) n) / (float)NUM_SAMPLES));
-        fi[n] = int_2_Q15(0);
+        fft.fr[n] = float_2_Q15(amp_scale * sin(dfl_scale * 6.283 * ((float) n) / (float)NUM_SAMPLES));
+        fft.fi[n] = int_2_Q15(0);
     }
 
-    FFTfix(fr,fi);
-    int max_bin = FFTmag(fr,fi);
+    FFT_fixdpt(&fft);
+    int max_bin = FFT_mag(&fft);
 
-    for(int n = 0; n < (NUM_SAMPLES >> 1); ++n){
-        printf("Bin %3u -> %f", n, Q15_2_float(fr[n]));
+    for(int n = 0; n < (fft.num_samples >> 1); ++n){
+        printf("Bin %3u -> %f\t%f", n, Q15_2_float(fft.fr[n]), Q15_2_float(fft.fi[n]));
         if(n == max_bin){
             printf("\tMAX");
         }
