22035b944bc5259bd5064b704e66cc5280f3e76b
4n3m4i1 <adrian.de.vico@gmail.com>
2024-02-01 12:18:16 -0800

    Fast FIR working w/ circular buffers library for any 2^n

--- src/tests/RP2040_Evaluation/core_0_exec.h
+++ src/tests/RP2040_Evaluation/core_0_exec.h
@@ -5,10 +5,15 @@
 #include "Memory_Management.h"
 #include "FFT/fft_half.h"
 
-    void core_0_main();
-    void setup_tiny_usb();
-    void execute_fft(FFT_PARAMS *fft);
-    void post_process_fft_data(FFT_PARAMS *fft);
-    void transmit_final_results(FFT_PARAMS *fft);
+
+void core_0_main();
+
+void setup_tiny_usb();
+
+void execute_fft(FFT_PARAMS *fft);
+
+void post_process_fft_data(FFT_PARAMS *fft);
+
+void transmit_final_results(FFT_PARAMS *fft);
 
 #endif
--- src/tests/RP2040_Evaluation/core_1_exec.h
+++ src/tests/RP2040_Evaluation/core_1_exec.h
@@ -5,10 +5,12 @@
 #include "Memory_Management.h"
 
 
-void core_1_main();
-void setup_ADC();
-void setup_sampling_ISR();
-void sampling_ISR_func();
+void    core_1_main();
+void    setup_ADC();
+void    setup_sampling_ISR();
+void    sampling_ISR_func();
+int     run_adaptive_taps();
+void    handle_intercore_h_hat_transfer();  // DMA tap data to other core, or error on overrun, start fft core 0
 
     
 
