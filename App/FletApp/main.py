cd0e1a70e83b38a3e6729877f67beb6d22bc6f5e
Christian Abella <christian.abella38@gmail.com>
2024-02-28 16:47:50 -0800

    fixes

--- App/FletApp/main.py
+++ App/FletApp/main.py
@@ -1,6 +1,8 @@
 from generic_include import *
 import serial
 
+import asyncio
+
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
@@ -14,6 +16,7 @@ import flet as ft
 from flet.matplotlib_chart import MatplotlibChart
 
 from multiprocessing import Process
+from threading import Thread
 from queue import Queue, Empty
 dataPort = "/dev/tty.usbmodem1234561"
 ctrlPort = "/dev/tty.usbmodem1234563"
@@ -23,6 +26,7 @@ NUM_VALUES = 128
 init_graph = [0 for i in range(NUM_VALUES)]
 
 FFT_real_queue = Queue()
+FFT_converted_queue = Queue()
 
 def serial_read(dataPortName, ctrlPortName, data_Queue: Queue):
     BYTES_PER_NUMBER = 2
@@ -77,10 +81,10 @@ def serial_read(dataPortName, ctrlPortName, data_Queue: Queue):
                 STATE = H
 
 
-def update_graph(Graph_Object, line, data_Queue: Queue):
+def raw_data_to_float_converter(data_out_Queue: Queue, data_in_Queue: Queue):
     while True:
         try:
-            graph_data_raw = data_Queue.get(block=False)
+            graph_data_raw = data_in_Queue.get(block=False)
             unpacked_graph_data = struct.iter_unpack('h', graph_data_raw)
             listed_graph_data = list(chain.from_iterable(unpacked_graph_data))
             converted_graph_data = Q15_to_float_array(listed_graph_data, 128)
@@ -88,29 +92,47 @@ def update_graph(Graph_Object, line, data_Queue: Queue):
             y_fft = fft.fft(converted_graph_data)
             y_fft = np.abs(y_fft)
 
-            print(y_fft)
+            # print(len(y_fft))
 
-            line.clear()
-            line.plot(y_fft)
-            # Graph_Object.canvas.draw()
-            # Graph_Object.canvas.flush_events()
+            data_out_Queue.put(y_fft, block=False)
+
         except Empty:
             continue
 
 def main(page: ft.Page):
+    page.title = "BODE GUI TESTING"
 
+    running = False
 
     figure = plt.figure()
     ax = figure.add_subplot()
-    ax.plot(init_graph)
+    line, = ax.plot(init_graph)
     chart = MatplotlibChart(figure, isolated=True, expand=True)
-    page.add(chart)
+
+    def handle_start_button_clicked(e):
+        running = True
+        serial_reader.start()
+        data_converter_process.start()
+
+        while running:
+            try:
+                line.set_ydata(FFT_converted_queue.get())
+                plt.draw()
+                # plt.pause(0.1)
+                chart.update()
+            except Empty:
+                continue
+        
+    page.add(ft.OutlinedButton(
+        width=150,
+        on_click=handle_start_button_clicked
+    ), 
+    chart)
     
-    serial_reader = Process(target=serial_read, args=(dataPort, ctrlPort, FFT_real_queue))
-    graph_updater_thread = Process(target=update_graph, args=(figure, ax, FFT_real_queue))
+    serial_reader = Thread(target=serial_read, args=(dataPort, ctrlPort, FFT_real_queue))
+    data_converter_process = Thread(target=raw_data_to_float_converter, args=(FFT_converted_queue, FFT_real_queue))
 
-    serial_reader.start()
-    graph_updater_thread.start()
 
+    
 
 ft.app(target=main)
