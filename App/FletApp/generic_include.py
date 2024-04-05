1155ba45bb2d2feac3037f3579256efb309720c2
Christian Abella <christian.abella38@gmail.com>
2024-04-05 16:16:29 -0700

    first go

--- App/FletApp/generic_include.py
+++ App/FletApp/generic_include.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 FL_CVT_CONST = 32768.0
 
 def Q15_to_float(a):
@@ -5,3 +7,17 @@ def Q15_to_float(a):
 
 def Q15_to_float_array(a, LENGTH):
     return [Q15_to_float(a[i]) for i in range(LENGTH)]
+
+class BANDIT_SETTINGS_BYTES(Enum):
+    BS_EN = 0
+    BS_AUTORUN = 1
+    BS_AUTOSEND = 2
+    BS_WGN_ALWAYS_ON = 3
+    BS_RESERVED = 4
+    BS_ERR_LSB = 5
+    BS_EFF_MSB = 6
+    BS_TAPLEN_LSB = 7
+    BS_TAPLEN_MSB = 8
+    BS_F_FRANGE = 9
+SETTINGS_BF_LEN = 10
+INIT_SETTINGS = bytes([0x1, 0x1, 0x1, 0x00, 0x00, 0x66, 0x06, 0x20, 0x00, 0x00])
