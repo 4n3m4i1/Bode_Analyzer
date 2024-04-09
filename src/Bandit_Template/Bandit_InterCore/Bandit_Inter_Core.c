5e47a499a318047cd3e7b85c19c89b630dec7248
4n3m4i1 <adrian.de.vico@gmail.com>
2024-04-09 13:20:56 -0700

    Fixed intercore (spec: messaging) library organization

--- src/Bandit_Template/Bandit_InterCore/Bandit_Inter_Core.c
+++ src/Bandit_Template/Bandit_InterCore/Bandit_Inter_Core.c
@@ -16,19 +16,6 @@ void Release_Lock(uint32_t lock_num){
     *((io_rw_32 *)(SIO_BASE + SIO_SPINLOCK0_OFFSET + (lock_num * 4))) = 0xDEADBEEF;
 }
 
-
-enum BANDIT_INTERCORE_MESSAGES {
-    BICM_NOP0,
-    BICM_NOP1,
-    BICM_NOP2,
-    BICM_NOP3,
-    BICM_NOP4,
-    BICM_NOP5,
-    BICM_NOP6,
-    BICM_NOP7,
-    BICM_RELEASE_DEBUG_REPORT_LOCK
-};
-
 inline void Send_FIFO_Message(uint32_t data_2_send){
     *((io_wo_32 *)(SIO_BASE + SIO_FIFO_WR_OFFSET)) = data_2_send;
 }
--- src/Bandit_Template/Bandit_InterCore/Bandit_Inter_Core.h
+++ src/Bandit_Template/Bandit_InterCore/Bandit_Inter_Core.h
@@ -8,9 +8,28 @@
 #define INTERCORE_FFTMEM_LOCK_A         26
 #define INTERCORE_CORE_1_DBG_LOCK       27
 
+/*
+    For use with intercore messaging,
+        triggers for Debug semaphore access requests
+*/
+
+enum BANDIT_INTERCORE_MESSAGES {
+    BICM_NOP0,
+    BICM_NOP1,
+    BICM_NOP2,
+    BICM_NOP3,
+    BICM_NOP4,
+    BICM_NOP5,
+    BICM_NOP6,
+    BICM_NOP7,
+    BICM_RELEASE_DEBUG_REPORT_LOCK
+};
 
 void Setup_Semaphores();
 bool Acquire_Lock_Blocking(uint32_t lock_num);
 void Release_Lock(uint32_t lock_num);
+
+void Send_FIFO_Message(uint32_t data_2_send);
+uint32_t Read_FIFO_Message();
 
 #endif
